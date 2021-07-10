125879449985db99cff16f1a65397f264627d186
lilitharcana <lilithalicecrawford@gmail.com>
2021-07-10 00:51:23 -0500

    Fix typo in comment

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 
 follow this guide to set up VS Code with SDL2: https://giovanni.codes/setup-sdl2-with-visual-studio-code-and-mingw64-on-windows/
 If you want SDL2_ttf, SDL2_image, etc, you must download the MinGW development libraries & also the x64/x86 runtime binaries to do the same:
-1.) in tasks.json, under "tasks"/"args", add the .../include/SDL2 folder with -I:
+1.) in tasks.json, under "tasks"/"args", add the ~/include/SDL2 folder with -I:
 	"-IC:/Users/lilit/Dev/Libraries/SDL2-mingw/SDL2-2.0.14/x86_64/include/SDL2"
-2.) also under "tasks"/"args", add the .../lib folder with -L:
+2.) also under "tasks"/"args", add the ~/lib folder with -L:
 	"-LC:/Users/lilit/Dev/Libraries/SDL2-mingw/SDL2-2.0.14/x86_64/lib",
 3.) again under "args", add the .lib files to the linker with -l:
 	"-lSDL2main", "-lSDL2", "-lSDL2_ttf", "-lSDL2_image", etc...
-4.) in c_cpp_properties.json, under "configurations"/"includePath", add your ..\\include\\SDL2 folders to update IntelliSense:
+4.) in c_cpp_properties.json, under "configurations"/"includePath", add your ~\\include\\SDL2 folders to update IntelliSense:
 	"C:\\Users\\lilit\\Dev\\Libraries\\SDL2-mingw\\SDL2-2.0.14\\x86_64\\include\\SDL2"
 5.) in your build folder, copy all the runtime .dll binaries:
 	SDL2.dll, SDL2_ttf.dll, zlib1.dll, libfreetype-6.dll, SDL2_image.dll, libpng16-16.dll, libjpeg-9.dll, libtiff-5.dll, libwebp-7.dll, etc...
